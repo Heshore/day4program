9107d72849ab96a88b278a207367f61b80de3ffb
Heshore R <rheshore20@gmail.com>
2025-03-21 10:47:24 +0530

    Add files via upload

---
+++ Ex2.java
@@ -0,0 +1,25 @@
+package Exception;
+class Ex2 {
+	static void checkAge(int age) {
+		if (age < 18) {
+			throw new IllegalArgumentException("Age must be 18 or older.");
+			
+		}
+		System.out.println("Access granted.");
+}
+	public static void main(String[] args) {
+		 try {
+			checkAge(16);
+		 }catch (IllegalArgumentException e) {
+			 System.out.println("Caught Exception: "+e.getMessage());
+		 }
+	}
+	}
+
+
+
+
+
+
+
+
